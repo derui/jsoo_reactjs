fc2ad846f0e36daac6f02d38ee471b889eaa809c
derui <derutakayu@gmail.com>
2018-02-11 13:51:15 +0900

    fix: add wrapper for Jstable.remove

--- src/roo_ref_table.ml
+++ src/roo_ref_table.ml
@@ -3,3 +3,4 @@
 let add table ~key ~value = Jstable.add table (Js.string key) value
 let keys table = Jstable.keys table |> List.map Js.to_string
 let find table ~key = Jstable.find table (Js.string key) |> Js.Optdef.to_option
+let remove table ~key = Jstable.remove table @@ Js.string key
