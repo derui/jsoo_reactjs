1ee23a793a97a598f6802d30ae338b5e10efaea3
derui <derutakayu@gmail.com>
2018-02-11 11:29:28 +0900

    fix: add original source of roo_raw

---
+++ src/roo_raw.js
@@ -0,0 +1,64 @@
+
+function _createClass(React, spec) {
+
+  return class extends React.Component {
+    constructor(props) {
+      super(props);
+
+      if (spec.constructor) {
+        spec.constructor.call(this, props);
+      }
+    }
+
+    componentWillMount() {
+      if (spec.componentWillMount) {
+        spec.componentWillMount.call(this);
+      }
+    }
+
+    componentDidMount() {
+      if (spec.componentDidMount) {
+        spec.componentDidMount.call(this);
+      }
+    }
+
+    render() {
+      if (spec.render) {
+        return spec.render.call(this);
+      }
+
+      throw new Error("render function must define in component spec");
+    }
+
+    componentWillReceiveProps(nextProps) {
+      if (spec.componentWillReceiveProps) {
+        spec.componentWillReceiveProps.call(this, nextProps);
+      }
+    }
+
+    shouldComponentUpdate(newProps, newState) {
+      if (spec.shouldComponentUpdate) {
+        return spec.shouldComponentUpdate.call(this, newProps, newState);
+      }
+      return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+      if (spec.componentWillUpdate) {
+        spec.componentWillUpdate.call(this, nextProps, nextState);
+      }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+      if (spec.componentDidUpdate) {
+        spec.componentDidUpdate.call(this, prevProps, prevState);
+      }
+    }
+
+    componentWillUnmount() {
+      if (spec.componentWillUnmount) {
+        spec.componentWillUnmount.call(this);
+      }
+    }
+  }
+}
--- src/roo_raw.ml
+++ src/roo_raw.ml
@@ -4,17 +4,18 @@
 *)
 let react_create_class_raw = {|
 (function () {
-  "use strict";
+"use strict";
 
-  var _createClass2 = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _createClass2 = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-  function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-  function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-  return function _createClass(React, spec) {
+  return function (React, spec) {
+
     return function (_React$Component) {
       _inherits(_class, _React$Component);
 
@@ -50,7 +51,7 @@ let react_create_class_raw = {|
             return spec.render.call(this);
           }
 
-          throw new Error("render must define in component spec");
+          throw new Error("render function must define in component spec");
         }
       }, {
         key: "componentWillReceiveProps",
@@ -61,22 +62,12 @@ let react_create_class_raw = {|
         }
       }, {
         key: "shouldComponentUpdate",
-        value: function (_shouldComponentUpdate) {
-          function shouldComponentUpdate(_x, _x2) {
-            return _shouldComponentUpdate.apply(this, arguments);
-          }
-
-          shouldComponentUpdate.toString = function () {
-            return _shouldComponentUpdate.toString();
-          };
-
-          return shouldComponentUpdate;
-        }(function (nextProps, nextState) {
+        value: function shouldComponentUpdate(newProps, newState) {
           if (spec.shouldComponentUpdate) {
-            return spec.shouldComponentUpdate.call(this, nextProps, nextState);
+            return spec.shouldComponentUpdate.call(this, newProps, newState);
           }
           return true;
-        })
+        }
       }, {
         key: "componentWillUpdate",
         value: function componentWillUpdate(nextProps, nextState) {
